98b0797803a1c82f34776c3f4c4a174ff356ee0c
sarthak trivedi <sarthaktrivedi386@gmail.com>
2026-04-12 12:11:02 +0530

    ADD: deployed backend url

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = "https://todo-webapp-pc.onrender.com"
 
 const authScreen = document.getElementById("auth-screen")
 const appScreen = document.getElementById("app-screen")
